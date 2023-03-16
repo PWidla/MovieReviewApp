1caa7c8b21ffc196f2285a18bb732b4f5cbb66e1
Patryk <patrykwidlazb@gmail.com>
2023-03-16 13:39:16 +0100

    Add models

---
+++ MovieReviewApp/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieReviewApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
