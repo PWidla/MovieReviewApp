2e9cd19fd3333d736c658aa0e6c29974f2229ea5
Patryk <patrykwidlazb@gmail.com>
2023-03-20 21:47:50 +0100

    Create director controller and its constructor

---
+++ MovieReviewApp/Controllers/DirectorController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MovieReviewApp.Data;
+
+namespace MovieReviewApp.Controllers
+{
+    public class DirectorController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly Mapper _mapper;
+
+        public DirectorController(DataContext context, Mapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+    }
+}
