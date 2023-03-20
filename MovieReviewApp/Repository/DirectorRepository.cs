d07fb72a4689d39be291decaf190865347ede820
Patryk <patrykwidlazb@gmail.com>
2023-03-20 20:50:44 +0100

    Create director repository

---
+++ MovieReviewApp/Repository/DirectorRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MovieReviewApp.Data;
+using MovieReviewApp.Interfaces;
+using MovieReviewApp.Models;
+
+namespace MovieReviewApp.Repository
+{
+    public class DirectorRepository : IDirectorRepository
+    {
+        private readonly DataContext _context;
+        private readonly Mapper _mapper;
+
+        public DirectorRepository(DataContext context, Mapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public bool DirectorExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Director GetDirector(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Director> GetDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Director> GetDirectorOfMovie(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Movie> GetMovieByDirector(int directorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
