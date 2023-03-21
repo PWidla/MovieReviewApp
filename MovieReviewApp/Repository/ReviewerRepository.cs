871027f13b09ab322ff800eaf3c1e615566f9ab0
Patryk <patrykwidlazb@gmail.com>
2023-03-21 16:55:06 +0100

    Create review repository and its interface

---
+++ MovieReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MovieReviewApp.Data;
+using MovieReviewApp.Interfaces;
+using MovieReviewApp.Models;
+
+namespace MovieReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        private readonly object _mapper;
+
+        public ReviewerRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
