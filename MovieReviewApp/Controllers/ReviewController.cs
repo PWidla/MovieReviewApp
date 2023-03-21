978f0b3c60c2df42aa391a1356dda3ba6159b4ce
Patryk <patrykwidlazb@gmail.com>
2023-03-21 15:19:30 +0100

    Create reviewdto and review controller

---
+++ MovieReviewApp/Controllers/ReviewController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MovieReviewApp.Dto;
+using MovieReviewApp.Interfaces;
+using MovieReviewApp.Models;
+using MovieReviewApp.Repository;
+using System.ComponentModel;
+
+namespace MovieReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_movieRepository.GetMovies());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(movies);
+        }
+    }
+}
