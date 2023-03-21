4b09b919f7e85028d1a30f857f1cd977902c1fa5
Patryk <patrykwidlazb@gmail.com>
2023-03-21 09:31:21 +0100

    Wire up director repository to program.cs

--- MovieReviewApp/Program.cs
+++ MovieReviewApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IDirectorRepository, DirectorRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
