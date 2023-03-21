c12cca4722942cea0d8bc8e0147311acf04c3664
Patryk <patrykwidlazb@gmail.com>
2023-03-21 17:54:59 +0100

    Wire up repositories to program.cs

--- MovieReviewApp/Program.cs
+++ MovieReviewApp/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 builder.Services.AddScoped<IDirectorRepository, DirectorRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IReviewerRepository, ReviewerRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
